63b57573cb034fce8d0bcf6d2e17376b4c0ab770
akash-soni <akash.200287@gmail.com>
2021-12-09 06:34:54 +0530

    tox setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+# find_packages will find all the packages
+
+setup(
+    name="src",
+    version = "0.0.1",
+    description = "its a wine 0 package",
+    author = "akash",
+    packages = find_packages(),
+    license = "MIT"
+)
